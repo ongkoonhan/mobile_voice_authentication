d0bc6531ceb07c53a725f4db13ee02a70ce5bee9
qinghui-ng <43191148+qinghui-ng@users.noreply.github.com>
2020-04-09 12:43:02 +0800

    Update to show Stats

--- voice-auth-client/VoiceAuth/app/src/main/java/com/example/voiceauth/Final.java
+++ voice-auth-client/VoiceAuth/app/src/main/java/com/example/voiceauth/Final.java
@@ -2,7 +2,12 @@ package com.example.voiceauth;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class Final extends AppCompatActivity {
 
@@ -10,5 +15,10 @@ public class Final extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_final);
+        Intent i = getIntent();
+        TextView tv = findViewById(R.id.Verifylog);
+        Bundle args = i.getBundleExtra("BUNDLE");
+        ArrayList<String> object = (ArrayList<String>) args.getSerializable("ARRAYLIST");
+        tv.setText(object.toString());
     }
 }
